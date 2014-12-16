69306350bf8ba1a3fb6bd4b87e67f814c8d3304c
tahashahid <m.tahashahid@yahoo.com>
2014-12-16 20:01:31 +0500

    ==== stable code ==== new features. - play next - play previous - custom progressbar local audios and videos are playing improve events and add new events.

--- demo/js/angular-mediagular.js
+++ demo/js/angular-mediagular.js
@@ -31,26 +31,37 @@
                     },
                     events: {
                         onReady: function(evt){
-                            evt.target.playVideo();
-                            evt.target.d.style.display = 'none';
-                            evt.target.d.style.pointerEvents = 'none';
+                            var target = evt.target;
+                            target.playVideo();
+                            target.d.style.display = 'none';
+                            target.d.style.pointerEvents = 'none';
+                            target.d.parentNode.style.display = "block";
                         },
                         onStateChange: function(evt){
                             switch (evt.data){
                                 case -1:
-                                    $rootScope.$broadcast("mg.stop");
+                                    $rootScope.$apply(function(){
+                                        $rootScope.$broadcast("mg.stop");
+                                    });
                                     evt.target.d.style.display = 'none';
                                     break;
                                 case 0:
-                                    $rootScope.$broadcast("mg.endMedia");
+                                    $rootScope.$apply(function(){
+                                        $rootScope.$broadcast("mg.endMedia");
+                                    });
                                     evt.target.d.style.display = 'none';
                                     break;
                                 case 1:
-                                    $rootScope.$broadcast("mg.playing");
+                                    $rootScope.$apply(function(){
+                                        $rootScope.$broadcast("mg.playing", "youtube");
+
+                                    });
                                     evt.target.d.style.display = 'block';
                                     break;
                                 case 2:
-                                    $rootScope.$broadcast("mg.paused");
+                                    $rootScope.$apply(function(){
+                                        $rootScope.$broadcast("mg.paused");
+                                    });
                                     break;
                             }
                         }
@@ -61,29 +72,35 @@
         })
         .run(function($templateCache){
             $templateCache.put("ngMediagular.html",
-                    '<audio id="audioplayer"></audio>'
-                    + '<div id="youtubePlayer"></div>'
+                '<style>#youtubePlayer{max-height: 100%;max-width: 100%;height: 250px}</style>'
+                + '<div style="display: none;height: 250px">'
+                + '<video id="audioplayer" poster="{{player.currentlyPlaying.poster}}"></video>'
+                + '<div id="youtubePlayer"></div>'
+                + '</div>'
+                + '<div ng-transclude></div>'
             )
         })
         .directive("ngMediagular", function($rootScope){
             return {
                 restrict: "AEC",
                 templateUrl: "ngMediagular.html",
+                transclude: true,
                 link: function(scope, element, attr){
                     var player = $rootScope.mediagular[attr.name || "player"] = {};
-                    player.currentIndex = 0;
+                    scope.player = player;
+                    player.currentIndex = -1;
                     player.currentPlayer = '';
-                    var $audioPlayer = element.children("#audioplayer");
+                    var $audioPlayer = angular.element(element[0].querySelector("#audioplayer"));
                     var audioPlayer = $audioPlayer[0];
 
                     player.scAPI = attr.scapi;
                     player.playlist = [
 
                         /*sample youtube obj
-                        {
-                            src: "qUJYqhKZrwA",
-                            provider: "youtube"
-                        }*/
+                         {
+                         src: "qUJYqhKZrwA",
+                         provider: "youtube"
+                         }*/
                         /*
                          sample soundcloudobj
                          {
@@ -91,28 +108,35 @@
                          provider: "soundcloud"
                          }*/
 
-                        {
-                            src: "https://api.soundcloud.com/tracks/56542489/stream",
-                            provider: "soundcloud"
-                        },
-                        {
-                            src: "qUJYqhKZrwA",
-                            provider: "youtube"
-                        },
+                        /* {
+                         src: "https://api.soundcloud.com/tracks/56542489/stream",
+                         provider: "soundcloud"
+                         },
+                         {
+                         src: "qUJYqhKZrwA",
+                         provider: "youtube"
+                         },
 
-                        {
-                            src: "qUJYqhKZrwA",
-                            provider: "youtube"
-                        },
+                         {
+                         src: "qUJYqhKZrwA",
+                         provider: "youtube"
+                         },
 
-                        {
-                            src: "https://api.soundcloud.com/tracks/163038821/stream",
-                            provider: "soundcloud"
-                        }
+                         {
+                         src: "https://api.soundcloud.com/tracks/163038821/stream",
+                         provider: "soundcloud"
+                         }*/
                     ];
 
                     $audioPlayer.on("ended", function(){
-                        $rootScope.$broadcast("mg.endMedia")
+                        $rootScope.$apply(function(){
+                            $rootScope.$broadcast("mg.endMedia")
+                        })
+                    });
+                    $audioPlayer.on("playing", function(){
+                        $rootScope.$apply(function(){
+                            $rootScope.$broadcast("mg.playing", player.currentPlayer)
+                        })
                     });
 
                     player.play = function(){
@@ -171,8 +195,41 @@
                         }
                         $rootScope.$broadcast("mg.stop");
                     };
+                    player.next = function(){
+                        player.currentIndex++;
+                        player.playQue();
+                    };
+                    player.previous = function(){
+                        player.currentIndex--;
+                        player.playQue();
+                    };
+
+                    player.duration = function(){
+                        if(player.currentPlayer == "soundcloud"){
+                            return audioPlayer.duration;
+                        }else if(player.currentPlayer == "youtube"){
+                            return youtubePlayer.getDuration();
+                        }else{
+                            return 0;
+                        }
+                    };
+
+                    player.seek = function(position){
+                        if(player.currentPlayer == "soundcloud"){
+                            audioPlayer.currentTime = position;
+                        }else if(player.currentPlayer == "youtube"){
+                            youtubePlayer.seekTo(position);
+                        }else{
+                            return false;
+                        }
+                        clearInterval(player.interval);
+
+                    };
+
+
 
                     player.playSoundCloud = function(src, autoplay){
+                        player.currentlyPlaying.poster = player.currentlyPlaying.img_src;
                         $audioPlayer.attr("autoplay", autoplay || '');
                         $audioPlayer.attr("src", src + '?client_id=' + player.scAPI);
                         player.currentPlayer = "soundcloud";
@@ -186,6 +243,7 @@
                     player.playMedia = function(mediaObj){
                         player.stop();
                         player.singlePlay = true;
+                        player.currentlyPlaying = mediaObj;
                         if(mediaObj.provider == "youtube"){
                             player.playYoutube(mediaObj.src, true)
                         }else if(mediaObj.provider == "soundcloud"){
@@ -196,14 +254,19 @@
                     player.playQue = function(){
                         player.stop();
                         player.singlePlay = false;
-                        var media = player.playlist[player.currentIndex];
-                        $rootScope.$broadcast("mg.playNext", media);
+                        player.currentlyPlaying = player.playlist[player.currentIndex];
+
+                        $rootScope.$broadcast("mg.playNext", player.currentlyPlaying );
                     };
 
                     player.startQue = function(){
                         player.currentIndex = 0;
                         player.playQue();
-                    }
+                    };
+
+                    player.clearProgress = function(){
+                        clearInterval(player.interval);
+                    };
 
                     scope.$on("mg.endMedia", function(){
                         var playingType = player.singlePlay ? "single" : "que";
@@ -229,15 +292,81 @@
                         }
                         player.currentIndex++;
                         var media = player.playlist[player.currentIndex];
+                        player.currentlyPlaying = player.playlist[player.currentIndex];
                         $rootScope.$broadcast("mg.playNext", media);
                     });
 
                     scope.$on("mg.Stop", function(event){
                         player.currentIndex = 0;
                         player.currentPlayer = '';
-                    })
+                    });
 
+                    scope.$on("mg.playing", function(){
+                        if(player.currentPlayer == "youtube"){
+                            element[0].querySelector("#youtubePlayer").style.display = "block";
+                            element[0].querySelector("#audioplayer").style.display = "none";
+                            youtubeProgressBar($rootScope, youtubePlayer, player);
+                        }else{
+                            element[0].querySelector("#youtubePlayer").style.display = "none";
+                            element[0].querySelector("#audioplayer").style.display = "block";
+                            progressBar($rootScope, audioPlayer, player)
+                        }
+                        player.playing = true;
+                    });
+                    scope.$on("mg.play", function(){
+                        player.playing = false;
+                    });
+                    scope.$on("mg.paused", function(){
+                        player.playing = false;
+                        clearInterval(player.interval);
+                    });
+                    scope.$on("mg.stop", function(){
+                        player.playing = undefined;
+                        clearInterval(player.interval);
+                    });
+                    scope.$on("mg.end.single", function(){
+                        player.playing = undefined;
+                        clearInterval(player.interval);
+                    });
+                    scope.$on("mg.playing", function(){
+                        player.playing = true;
+                    });
+                    scope.$on("mg.ended", function(){
+                        player.playing = undefined;
+                        clearInterval(player.interval);
+                    });
                 }
             }
         })
+
+    /*helpful methods*/
+    function youtubeProgressBar($rootScope, ytPlayer, player){
+        player.clearProgress();
+        player.interval = window.setInterval(function(){
+
+            $rootScope.$apply(function(){
+                player.progress = ytPlayer.getCurrentTime();
+                player.progressPercent = (ytPlayer.getCurrentTime() / ytPlayer.getDuration())*100;
+                console.log(player.progressPercent)
+            })
+
+
+
+        },600);
+    }
+
+    function progressBar ($rootScope, mPlayer, player){
+        player.clearProgress();
+        player.interval = window.setInterval(function(){
+
+            $rootScope.$apply(function(){
+                player.progress = mPlayer.currentTime;
+                player.progressPercent = (mPlayer.currentTime / mPlayer.duration)*100;
+                console.log(player.progressPercent)
+            })
+
+
+        },600);
+    }
+    /*helpful methods*/
 })();
